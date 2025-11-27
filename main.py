9fb36c983638de9937645ce4026fb3e9fe3fb094
Tom LECOURT <tom.lecourt@edu.esiee.fr>
2025-11-27 08:14:07 +0000

    update v2

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
+"""
+Main script for knowing if a number is prime or not
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """Show if a number is prime or not.
+    Args:
+        p (float) : the chosen number.
+    Returns:
+        boolean: result if he is prime or not.
+    """
 
-    # votre code ici
-
-    pass
+    if p < 2:
+        return False
+    if p == 2:
+        return True
+    for i in range(2, int(sqrt(p)+1)):
+        if p % i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
+    """main function 
+    """
 
     # vos appels à la fonction secondaire ici
 
